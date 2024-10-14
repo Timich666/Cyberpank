4191115af88493c52cf831603e33337221458b81
Timich66 <sempailove342@gmail.com>
2024-10-14 22:14:13 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+  })
+  
